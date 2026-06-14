977d2767a84dd09cf1d55c56f253728781c0aa4b
smrha <lord.smrha@gmail.com>
2026-06-14 19:47:34 +0330

    [ADD] waitlists app

--- src/home/settings.py
+++ src/home/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ninja_extra',
     'ninja_jwt',
+    'waitlists',
 ]
 
 MIDDLEWARE = [
---
+++ src/waitlists/schemas.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from ninja import Schema
+from pydantic import EmailStr
+
+class WaitlistEntryCreateSchema(Schema):
+    email: EmailStr
+
+class WaitlistEntryDetailSchema(Schema):
+    email: EmailStr
+    timestamp: datetime
