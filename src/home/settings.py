b220c79695b26611f6828e5a129b5944b2b0f90a
smrha <lord.smrha@gmail.com>
2026-06-15 18:52:12 +0330

    [ADD] django cors headers

--- src/home/settings.py
+++ src/home/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ninja_extra',
     'ninja_jwt',
+    "corsheaders",
     'waitlists',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,6 +56,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'home.urls'
+
+CORS_URLS_REGEX = r"^/api/.*$"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 TEMPLATES = [
     {
